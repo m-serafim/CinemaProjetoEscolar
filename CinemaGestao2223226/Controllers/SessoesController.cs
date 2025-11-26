27acee39c255ba27e95501d578b4721383807b55
M1G-L <miguel.ag.serafim2025@gmail.com>
2025-11-26 21:41:26 +0000

    Erro no Sessoes

---
+++ CinemaGestao2223226/Controllers/SessoesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CinemaGestao.Models;
+using CinemaGestao2223226.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CinemaGestao2223226.Controllers
+{
+    [Authorize(Roles = "Administrador")]
+    public class SessaosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SessaosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Sessaos
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Sessoes.Include(s => s.Filme);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Sessaos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sessao = await _context.Sessoes
+                .Include(s => s.Filme)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (sessao == null)
+            {
+                return NotFound();
+            }
+
+            return View(sessao);
+        }
+
+        
+        // GET: Sessoes/Create
+        public IActionResult Create()
+        {
+            ViewData["FilmeId"] = new SelectList(_context.Filmes, "Id", "Titulo");
+            return View();
+        }
+
+        // POST: Sessaos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FilmeId,DataHora,Sala,Preco,LugaresTotais,LugaresDisponiveis")] Sessao sessao)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sessao);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FilmeId"] = new SelectList(_context.Filmes, "Id", "Titulo", sessao.FilmeId);
+            return View(sessao);
+        }
+
+        // GET: Sessaos/Edit/5
+        // GET: Sessoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sessao = await _context.Sessoes.FindAsync(id);
+            if (sessao == null)
+            {
+                return NotFound();
+            }
+            ViewData["FilmeId"] = new SelectList(_context.Filmes, "Id", "Titulo", sessao.FilmeId);
+            return View(sessao);
+        }
+
+        // POST: Sessaos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FilmeId,DataHora,Sala,Preco,LugaresTotais,LugaresDisponiveis")] Sessao sessao)
+        {
+            if (id != sessao.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sessao);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SessaoExists(sessao.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FilmeId"] = new SelectList(_context.Filmes, "Id", "Descricao", sessao.FilmeId);
+            return View(sessao);
+        }
+
+        // GET: Sessaos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sessao = await _context.Sessoes
+                .Include(s => s.Filme)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (sessao == null)
+            {
+                return NotFound();
+            }
+
+            return View(sessao);
+        }
+
+        // POST: Sessaos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var sessao = await _context.Sessoes.FindAsync(id);
+            if (sessao != null)
+            {
+                _context.Sessoes.Remove(sessao);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SessaoExists(int id)
+        {
+            return _context.Sessoes.Any(e => e.Id == id);
+        }
+    }
+}
